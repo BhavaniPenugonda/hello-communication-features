a799ca294bf9613c9cfceb98d6bf31abb28abd35
BhavaniPenugonda <bhavani0202@gmail.com>
2025-01-09 14:20:06 +0100

    Two buttons are added to the view

--- App.js
+++ App.js
@@ -1,22 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
+
 import { StyleSheet, Text, View } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 
 
-export default function App() {
+const App=() =>{
+  const [image, setImage] = useState(null);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button
+        title="Pick an image from the library"
+        onPress={pickImage}
+      />
+      <Button
+        title= "Take a photo"
+        onPress={() => {}}
+      />
     </View>
   );
-}
+ }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: 'center'
+  }
 });
+
+export default App;
+  
