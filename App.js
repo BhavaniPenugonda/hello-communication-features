36e2e1b1f7f89a790d809c9f55d92da56bf60cac
BhavaniPenugonda <bhavani0202@gmail.com>
2025-01-09 14:33:15 +0100

    async functions are added for images

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
 
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet,Button,Image, View } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
+import { useState } from 'react';
 
 
 const App=() =>{
   const [image, setImage] = useState(null);
+  const pickImage = async () => {
+    let permissions = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (permissions?.granted) {
+       let result = await ImagePicker.launchImageLibraryAsync();
+
+      if (!result.canceled) setImage(result.assets[0]);
+      else setImage(null)
+    }
+  }
 
   return (
     <View style={styles.container}>
@@ -16,6 +27,9 @@ const App=() =>{
         title= "Take a photo"
         onPress={() => {}}
       />
+      {image &&
+        <Image source={{ uri: image.uri }} style={{ width: 200, height: 200 }} />
+      }
     </View>
   );
  }
@@ -23,7 +37,8 @@ const App=() =>{
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center'
+    justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
   }
 });
 
